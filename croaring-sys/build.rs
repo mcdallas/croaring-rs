f10ccd8caa1ffc2e1b3b97ff1a8da1dd2a93d87b
Mike Dallas <mcdallas@protonmail.com>
2018-12-16 22:17:55 +0000

    remove march=native

--- croaring-sys/build.rs
+++ croaring-sys/build.rs
@@ -7,7 +7,6 @@ use std::path::PathBuf;
 fn main() {
     cc::Build::new()
         .flag("-std=c11")
-        .flag("-march=native")
         .flag("-O3")
         .file("CRoaring/roaring.c")
         .compile("libroaring.a");
